3b150922c4a804ac7377e58a60450f9cb220759e
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-27 19:20:20 +0300

    47th ex

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿/*
-Console.WriteLine("Введите количество строк  ");
+﻿Console.WriteLine("Введите количество строк  ");
 int linesVol = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов");
 int columnsVol = Convert.ToInt32(Console.ReadLine());
@@ -30,54 +29,4 @@ void PrintArray(double[,] array)
         Console.Write("]");
         Console.WriteLine("");
     }
-}
-*/
-
-int rows = ReadInt("Введите количество строк: ");
-int colums = ReadInt("Введите количество столбцов: ");
-int[,] numbers = new int[rows, colums];
-int idRows = ReadInt("Введите строку: ");
-int idColums = ReadInt("Введите столбец: ");
-
-FillArray2D(numbers);
-PrintArray2D(numbers);
-
-if ((idRows < rows)&(idColums < colums))
-{
-    Console.WriteLine(numbers[idRows, idColums]);
-}
-else
-{
-    Console.WriteLine("Такого числа в массиве нет");
-}
-
-void FillArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
-    {
-        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
-        {
-            array[i, j] = new Random().Next(-9, 9);
-        }
-    }
-}
-
-void PrintArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
-    {
-        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-
-
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
 }
