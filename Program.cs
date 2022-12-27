41d940957d2708258a8f8d7ff1763086bfe54588
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-27 19:25:25 +0300

    52th ex

--- Program.cs
+++ Program.cs
@@ -33,6 +33,7 @@ void PrintArray(double[,] array)
 }
 */
 
+/*
 Console.Write("Введите строку: ");
 int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
 Console.Write("Введите столбец: ");
@@ -60,4 +61,28 @@ Console.WriteLine("Элемент не существует  ");
     Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
 }
 Console.ReadLine();
- 
+*/
+
+ Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("---------------------------");
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
